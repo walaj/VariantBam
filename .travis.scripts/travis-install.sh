f5b8ae85751d7997cba710b8d157ba9cf1be21cc
Jeremiah Wala <jwala@broadinstitute.org>
2016-07-10 17:49:26 -0400

    removed boost from travis

--- .travis.scripts/travis-install.sh
+++ .travis.scripts/travis-install.sh
@@ -3,9 +3,9 @@
 set -e
 set -o pipefail
 
-if [ -d "${BOOST_ROOT}" ]; then
-  (cd "${BOOST_ROOT}"
-    ./bootstrap.sh --with-libraries="${BOOST_LIBS}"
-    ./b2 threading=multi --prefix="${BOOST_ROOT}" -d0 install
-  )
+#if [ -d "${BOOST_ROOT}" ]; then
+#  (cd "${BOOST_ROOT}"
+#    ./bootstrap.sh --with-libraries="${BOOST_LIBS}"
+#    ./b2 threading=multi --prefix="${BOOST_ROOT}" -d0 install
+#  )
 fi
